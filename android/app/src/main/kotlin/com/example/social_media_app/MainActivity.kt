b67c7aa6ceffe42aee73a0ca4b866e00b57ac9e6
EnderPluser <95554298+EnderPluser@users.noreply.github.com>
2022-03-05 16:27:32 +0700

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/example/social_media_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/social_media_app/MainActivity.kt
@@ -1,6 +1,21 @@
 package com.example.social_media_app
 
+// MainActivity.kt
+-import android.os.Bundle
+-import io.flutter.app.FlutterActivity
++import io.flutter.embedding.android.FlutterActivity
+-import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+-  override fun onCreate(savedInstanceState: Bundle?) {
+-    super.onCreate(savedInstanceState)
+-    GeneratedPluginRegistrant.registerWith(this)
+
+
+
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
+    }
+  }
 }
